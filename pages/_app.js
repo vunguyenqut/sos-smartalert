0a5df768a38f388df1febacf2829e9804bc23e9e
vunguyenqut <vunguyenqut@gmail.com>
2022-05-24 15:31:53 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
+
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+        OneSignal.init({
+            appId: "75ff99fa-9290-49fc-9e58-272d1814433b",
+            notifyButton: {
+                enable: true,
+            },
+
+            allowLocalhostAsSecureOrigin: true,
+        });
+    });
+
+    return () => {
+        window.OneSignal = undefined;
+    };
+}, []); // <-- run this effect once on mount
+  
+  
   return <Component {...pageProps} />
 }
 
