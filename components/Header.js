43a52512c4e7ba458d81c073d5dbe7adbd42611b
vunguyenqut <vunguyenqut@gmail.com>
2022-05-24 15:27:08 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,14 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
+  
+  <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "75ff99fa-9290-49fc-9e58-272d1814433b",
+    });
+  });
+</script>
+  
 }
