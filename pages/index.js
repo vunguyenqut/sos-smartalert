2bc970d8b7828cbe907e47d29b794574d4bf7b94
vunguyenqut <vunguyenqut@gmail.com>
2022-05-24 15:19:51 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Smart Alert by S.O.S team" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Demo project by S.O.S team via Krystal's Hackamoon 
         </p>
       </main>
 
