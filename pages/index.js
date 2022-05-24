acde1c696f86800fd4f7a8be241fca0698642c52
vunguyenqut <vunguyenqut@gmail.com>
2022-05-24 15:59:00 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,12 @@ import React, { useEffect, useState } from "react";
   
 
 export default function Home() {
-  
-const [resultApi, setResultApi] = useState();
-useEffect(() => {
-  window.OneSignal = window.OneSignal || [];
-  OneSignal.push(function () {
-    OneSignal.init({
-      appId: "75ff99fa-9290-49fc-9e58-272d1814433b",
-      notifyButton: {
-        enable: true,
-      },
-
-      allowLocalhostAsSecureOrigin: true,
-    });
-  });
-//  return () => {
-//    window.OneSignal = undefined;
-//  };
-  
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""> </script>
-    
-    
       </Head>
 
       <main>
@@ -42,8 +22,5 @@ useEffect(() => {
 
       <Footer />
     </div>
-  ) => {
-    window.OneSignal = undefined;
-      };
-}, []);
+  ) 
 }
