744c022406c0cccb01078d780592d7be0778f5ac
vunguyenqut <vunguyenqut@gmail.com>
2022-05-24 15:40:44 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,7 @@
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-
-  useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-        OneSignal.init({
-            appId: "75ff99fa-9290-49fc-9e58-272d1814433b",
-            notifyButton: {
-                enable: true,
-            },
-
-            allowLocalhostAsSecureOrigin: true,
-        });
-    });
-
-    return () => {
-        window.OneSignal = undefined;
-    };
-}, []); // <-- run this effect once on mount
-  
-  
-  //return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default Application
