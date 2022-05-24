41000efd4d6421651ea0ab897bdfe2fced3f7b87
vunguyenqut <vunguyenqut@gmail.com>
2022-05-24 16:14:15 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,9 @@
-import '@styles/globals.css'
-import React, { useState, useEffect } from 'react';
+import "../styles/globals.css";
+import useOneSignal from "../utils/useOneSignal";
 
-
-    
-function Application() {
-useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-        OneSignal.init({
-            appId: "75ff99fa-9290-49fc-9e58-272d1814433b",
-            notifyButton: {
-                enable: true,
-            },
-
-            allowLocalhostAsSecureOrigin: true,
-        });
-    });
-
-    return () => {
-        window.OneSignal = undefined;
-    };
-}, []); // <-- run this effect once on mount
+function MyApp({ Component, pageProps }) {
+  useOneSignal();
+  return <Component {...pageProps} />;
 }
 
-
-//function Application({ Component, pageProps }) {
-//  return <Component {...pageProps} />
-//}
-
-//75ff99fa-9290-49fc-9e58-272d1814433b
-
-export default Application
+export default MyApp;
